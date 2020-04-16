506c116dc590a64e04ca83635e16912f7f98a631
sebycotoc98 <48725202+sebycotoc98@users.noreply.github.com>
2020-04-16 11:43:55 +0300

    Add files via upload
    add account settings package
    table for books, genres + tabele legatura

--- sql files/script_init_db.sql
+++ sql files/script_init_db.sql
@@ -4,10 +4,23 @@ DROP TABLE users CASCADE CONSTRAINTS;
 DROP TABLE friends CASCADE CONSTRAINTS;
 /
 
+DROP TABLE books CASCADE CONSTRAINTS;
+/
+
+DROP TABLE books_users CASCADE CONSTRAINTS;
+/
+
+DROP TABLE genres CASCADE CONSTRAINTS;
+/
+
+DROP TABLE genres_users CASCADE CONSTRAINTS;
+/
+
 CREATE TABLE users (
   id INT NOT NULL PRIMARY KEY,
   username VARCHAR2(50) NOT NULL UNIQUE,
   password VARCHAR2(200) NOT NULL,
+  description VARCHAR2(500) NOT NULL,
   created_at DATE,
   updated_at DATE
 )
@@ -22,6 +35,51 @@ CREATE TABLE friends (
   CONSTRAINT fk_friends_id_user1 FOREIGN KEY (id_user1) REFERENCES users(id),
   CONSTRAINT fk_friends_id_user2 FOREIGN KEY (id_user2) REFERENCES users(id),
   CONSTRAINT no_duplicates_friends UNIQUE (id_user1, id_user2)
+)
+/
+
+CREATE TABLE books (
+  id INT NOT NULL PRIMARY KEY,
+  title VARCHAR2(50) NOT NULL UNIQUE,
+  author VARCHAR2(50) NOT NULL,
+  id_genre INT NOT NULL,
+  an INT NOT NULL,
+  ISBN VARCHAR2(50) NOT NULL,
+  created_at DATE,
+  updated_at DATE
+)
+/
+
+CREATE TABLE books_users (
+--  id INT PRIMARY KEY,
+  u_username INT NOT NULL,
+  b_title INT NOT NULL,
+  created_at DATE,
+  updated_at DATE,
+  CONSTRAINT fk_booksusers_username FOREIGN KEY (u_username) REFERENCES users(username),
+  CONSTRAINT fk_booksusers_title FOREIGN KEY (t_title) REFERENCES books(title),
+  CONSTRAINT no_duplicates_booksusers UNIQUE (u_username, t_title)
+)
+/
+
+CREATE TABLE genres (
+  id INT NOT NULL PRIMARY KEY,
+  denumire VARCHAR2(50) NOT NULL UNIQUE,
+  created_at DATE,
+  updated_at DATE
+)
+/
+
+
+CREATE TABLE genres_users (
+--  id INT PRIMARY KEY,
+  u_username INT NOT NULL,
+  g_denumire INT NOT NULL,
+  created_at DATE,
+  updated_at DATE,
+  CONSTRAINT fk_genresusers_username FOREIGN KEY (u_username) REFERENCES users(username),
+  CONSTRAINT fk_genresusers_genre FOREIGN KEY (g_denumire) REFERENCES genres(denumire),
+  CONSTRAINT no_duplicates_genresusers UNIQUE (u_username, g_denumire)
 )
 /
 
