8e097bc7a66e2a62bc049cb8be3eaf74ddd9cc59
raluca-dascalu <48430103+raluca-dascalu@users.noreply.github.com>
2020-05-07 13:10:51 +0300

    Update profile.php

--- myreader/profile.php
+++ myreader/profile.php
@@ -29,6 +29,7 @@ $dbh = null;
     <head>
     <title>My reader</title>
 	<link rel="stylesheet" href="stylesheets/style.css">
+	<link rel="stylesheet" href="stylesheets/notifications.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800&amp;display=swap">
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Raleway:wght@100&amp;display=swap&quot; rel=&quot;stylesheet">
@@ -113,6 +114,7 @@ $dbh = null;
 				$dbh = null;
 				?>
 			</p>
+			<button class="send_request">Send request</button>
 		</center>
 		</div>
 	</body>
