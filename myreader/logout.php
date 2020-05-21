384e8ee461ef803a4066ff258ca100e5b1f00394
sebycotoc98 <48725202+sebycotoc98@users.noreply.github.com>
2020-05-21 13:57:26 +0300

    Add files via upload
    stergerea coordonatelor la delogare

--- myreader/logout.php
+++ myreader/logout.php
@@ -1,5 +1,8 @@
 <?php
-session_start();
+include("session.php");
+include('Includere/connection.php');
+$sql = "UPDATE `users` SET `last_latitude` = NULL , `last_longitude` = NULL WHERE `email` = '$email'";
+$datas = $dbh->query($sql);
 session_unset();
 session_destroy();
 echo "<script>location.href = 'login.php'</script>";
